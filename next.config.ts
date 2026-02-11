2264009e4c5752476ec454d83a52aca0a848c6ab
omsaidesaii <omsaidesai9@gmail.com>
2026-02-12 00:05:48 +0530

    updated config

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "cloud.appwrite.io",
       },
+      {
+        protocol: "https",
+        hostname: "fra.cloud.appwrite.io",
+      },
     ],
   },
 };
